a193fea5c7ae861a14dccc2a44e62e11cc1831ee
Jakub Czakon <jakub.czakon@pascal01.intra.codilime.com>
2017-12-22 20:17:23 +0100

    added aligner and clf architecture

--- minerva/whales/tasks.py
+++ minerva/whales/tasks.py
@@ -18,9 +18,41 @@ class Task1(Task):
         self.trainer.pipeline.get_step('localizer_network').is_substituted = True
         return self
 
-
 @register_task
 class Task2(Task):
+    """
+    Note:
+        Aligner Architecture
+    """
+
+    def modify_config(self, user_config):
+        self.trainer.config['aligner_network']['architecture_config']['model_params'] = user_config
+        return self
+
+    def modify_pipeline(self, user_solution, user_config):
+        self.trainer.pipeline.get_step('aligner_network').transformer._build_model = user_solution
+        self.trainer.pipeline.get_step('aligner_network').is_substituted = True
+        return self
+
+@register_task
+class Task3(Task):
+    """
+    Note:
+        Classifier Architecture
+    """
+
+    def modify_config(self, user_config):
+        self.trainer.config['classifier_network']['architecture_config']['model_params'] = user_config
+        return self
+
+    def modify_pipeline(self, user_solution, user_config):
+        self.trainer.pipeline.get_step('classifier_network').transformer._build_model = user_solution
+        self.trainer.pipeline.get_step('classifier_network').is_substituted = True
+        return self
+
+
+@register_task
+class Task4(Task):
     """
     Note:
         Localizer weight regularization
